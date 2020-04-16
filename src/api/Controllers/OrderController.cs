4b08a3c48e6790c544f706107115f2eb848c5942
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2020-04-16 12:57:40 -0300

    Initializes repository

---
+++ src/api/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> All()
+        {
+            return "Hello Stranger";
+        }
+
+        [HttpPost]
+        public async Task<string> Save()
+        {
+            return "Saved";
+        }
+    }
+}
---
+++ src/infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace infra
+{
+    public class Class1
+    {
+    }
+}
