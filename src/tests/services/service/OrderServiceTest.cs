93e0e0b5d4bfaa665986085f8e2850de193bc4b1
Ricardo da Verdade Silva <ricardovsilva@msn.com>
2020-04-17 11:32:52 -0300

    Adds service layer

---
+++ src/tests/services/service/OrderServiceTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests.services.service
+{
+    [TestClass]
+    public class OrderServiceTest
+    {
+        [TestMethod]
+        public void Create_GivenInvalidTimeOfDay()
+        {
+
+        }
+    }
+}
